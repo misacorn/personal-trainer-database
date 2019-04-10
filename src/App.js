70a494eb9a3b8e6340e9ec783662c01bf648308d
Misa <dieuhuongngo@gmail.com>
2019-04-10 10:01:24 +0300

    starting the app with App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import CustomerList from "./components/CustomerList";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              CUSTOMER LIST
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CustomerList />
+      </div>
+    );
+  }
+}
+
+export default App;
